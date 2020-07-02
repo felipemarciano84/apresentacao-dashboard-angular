089e1a30f1ca6533f7bde88b17f7e67d7beea961
fgisoltuionsweb <fgisolutionsweb.gmail.com>
2020-07-02 03:57:26 -0300

    primeiro commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { AsideBarComponent } from './pages/aside-bar/aside-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AsideBarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
